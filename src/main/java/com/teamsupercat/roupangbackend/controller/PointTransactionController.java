e0f3ea4da82f42e362f99cfad851e39dfd179874
dabin <113006964+dabinHam@users.noreply.github.com>
2023-08-25 02:03:41 +0900

    refactor : 안쓰는 import 및 미구현 주석코드 삭제
    안쓰는 import 및 미구현 주석코드 삭제

--- src/main/java/com/teamsupercat/roupangbackend/controller/PointTransactionController.java
+++ src/main/java/com/teamsupercat/roupangbackend/controller/PointTransactionController.java
@@ -2,20 +2,16 @@ package com.teamsupercat.roupangbackend.controller;
 
 import com.teamsupercat.roupangbackend.common.ResponseDto;
 import com.teamsupercat.roupangbackend.dto.CustomUserDetail.CustomUserDetail;
-import com.teamsupercat.roupangbackend.dto.point.*;
+import com.teamsupercat.roupangbackend.dto.point.ChargePoint;
 import com.teamsupercat.roupangbackend.service.PointTransactionService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @Api(tags = "포인트충전 API")
 @RestController
@@ -24,49 +20,7 @@ import javax.validation.Valid;
 public class PointTransactionController {
 
     private final PointTransactionService pointTransactionService;
-//
-//    @ApiOperation(value = "토큰가져오기")
-//    @PostMapping("/accesstoken")
-//    public ResponseEntity<AccessTokenResponse> getAccessToken(
-//            @AuthenticationPrincipal UserDetails userDetails,
-//            @RequestBody @Valid IampartDto iampartDto
-//    ) {
-//
-//        String email = userDetails.getUsername();
-//
-//        AccessTokenResponse accessTokenResponse = pointTransactionService.getAccessToken(email,iampartDto.getApiKey(),iampartDto.getApiSecret());
-//        return ResponseEntity.ok(accessTokenResponse);
-//    }
-//
-//    @ApiOperation(value = "포인트 충전 사전검증")
-//    @PostMapping("/prepare")
-//    public ResponseEntity<String> pointPrepare(@RequestBody @Valid PointPrepareRequest request) {
-//        return pointTransactionService.pointPrepare(request.getMerchantUid(), request.getAmount());
-//    }
 
-    // prepare (사전검증) : 주문번호 생성해야함
-    // 생성한 주문번호로 사전검증 완료되면 프론트로도 응답해주고
-    // 프론트에서 결제완료 처리되면
-
-    /*
-    url: "https://api.iamport.kr/payments/prepare",
-    method: "post",
-    headers: { "Content-Type": "application/json" },
-    data: {
-    merchant_uid: "...", // 가맹점 주문번호
-    amount: 420000, // 결제 예정금액
-    */
-
-    // merchant_uid:  내가 생성한 주문번호를 프론트에서 돌려받음
-    // merchant_uid을 돌려받으면
-    // https://developers.portone.io/docs/ko/auth/guide/5/post (사후검증 docs url)
-
-    // complete (사후검증)
-    // 응답받은 내용을 바탕으로 실 결제 금액과 결제요청금액(가맹점 자체 데이터베이스)을 비교
-    // 결제 상세내역 조회를 위해 포트원 결제 단건 조회 API 요청
-    // 포트원 결제고유번호(imp_uid), 가맹점 주문번호(merchant_uid)를 프론트엔드로부터 수신
-
-    // 사후검증 완료 되면 결제완료 처리 해주기.
 
     @ApiOperation(value = "포인트 충전")
     @PostMapping("/charge")
--- src/main/java/com/teamsupercat/roupangbackend/service/PointTransactionService.java
+++ src/main/java/com/teamsupercat/roupangbackend/service/PointTransactionService.java
@@ -2,7 +2,6 @@ package com.teamsupercat.roupangbackend.service;
 
 import com.teamsupercat.roupangbackend.common.CustomException;
 import com.teamsupercat.roupangbackend.common.ErrorCode;
-import com.teamsupercat.roupangbackend.dto.point.AccessTokenResponse;
 import com.teamsupercat.roupangbackend.dto.point.ChargePoint;
 import com.teamsupercat.roupangbackend.dto.point.PointDto;
 import com.teamsupercat.roupangbackend.entity.Member;
@@ -11,12 +10,8 @@ import com.teamsupercat.roupangbackend.repository.MemberRepository;
 import com.teamsupercat.roupangbackend.repository.PointTransactionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
@@ -25,74 +20,6 @@ public class PointTransactionService {
 
     private final MemberRepository memberRepository;
     private final PointTransactionRepository pointTransactionRepository;
-//    private final RestTemplate restTemplate;
-//
-//    private static final String API_BASE_URL = "https://api.iamport.kr";
-//
-//    // API 키와 시크릿을 클래스 필드로 선언
-//    private static final String API_KEY = "YOUR_API_KEY";
-//    private static final String API_SECRET = "YOUR_API_SECRET";
-
-
-//    @Transactional
-//    public AccessTokenResponse getAccessToken(String email, String apiKey, String apiSecret) {
-//
-//        Member member = memberRepository.findByEmail(email)
-//                .orElseThrow(()-> new CustomException(ErrorCode.IM_PORT_API_COMMUNICATION_ERROR));
-//
-//        log.info("{} member",member);
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-//        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
-//        params.add("imp_key", apiKey);
-//        params.add("imp_secret", apiSecret);
-//
-//
-//        log.info("{} apiKey", apiKey);
-//        log.info("{} apiSecret", apiSecret);
-//
-//        HttpEntity<?> entity = new HttpEntity<>(params, headers);
-//        log.info("{} apiKey", apiKey);
-//        log.info("{} apiSecret", apiSecret);
-//        ResponseEntity<AccessTokenResponse> response = restTemplate.exchange(
-//                API_BASE_URL + "/users/getToken", HttpMethod.POST, entity, AccessTokenResponse.class
-//        );
-//        log.info("{} response",response);
-//        // 응답이 200이면 성공 && 응답의 본문이 null이 아니면 응답 본문에 데이터가 포함
-//        if (response.getStatusCode() == HttpStatus.OK && response.getBody() != null) {
-//            return response.getBody();
-//        } else {
-//            throw new CustomException(ErrorCode.IM_PORT_API_COMMUNICATION_ERROR);
-//        }
-//
-//    }
-
-
-//    public ResponseEntity<String> pointPrepare(String merchantUid, int amount) {
-//        // merchantUid와 amount를 사용하여 사전검증 로직 수행
-//        // 여기에 사전검증 로직을 작성하고, 결과에 따라 적절한 응답을 생성
-//        // 예를 들어, 검증 성공 시 "Payment prepared successfully" 메시지를 반환하도록 구현
-//
-//        // merchantUid가 유효한지 확인
-//        if (!isMerchantUidValid(merchantUid)) {
-//            return ResponseEntity.badRequest().body("Invalid merchantUid");
-//        }
-//
-//        // amount가 0보다 큰지 확인
-//        if (amount <= 0) {
-//            return ResponseEntity.badRequest().body("Invalid amount");
-//        }
-//
-//        // 사전검증 성공
-//        return ResponseEntity.ok("Payment prepared successfully");
-//    }
-//
-//    private boolean isMerchantUidValid(String merchantUid) {
-//        // merchantUid 유효성 검사 로직을 구현
-//        return merchantUid.matches("^[a-zA-Z0-9-]+$");
-//    }
-
 
     @Transactional
     public PointDto chargePoints(
@@ -123,7 +50,5 @@ public class PointTransactionService {
         }
 
     }
-
-    // getUserPointsHistory
 
 
